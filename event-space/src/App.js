fcc88cd83b35bfff233986ee769e2521753f8842
Matthew Perrot <Matthew@MacBook-Pro.local>
2022-11-17 19:54:44 -0800

    imported font

--- event-space/src/App.js
+++ event-space/src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div className = "App">
+        <header className = "App-header">
+            <h1>Event Space</h1>
+            <div className="Div-2">
+                <p>Your key to events and shows in Las Vegas, NV!</p>
+            </div>
+            <div className="Div-3">
+                <h3>Test</h3>
+            </div>
+            <div className="Div-3">
+                <h3>Test</h3>
+            </div>
+        </header>
     </div>
-  );
+    )
 }
 
 export default App;
