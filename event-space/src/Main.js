eaced55e4b6ef946961748dafd77601a4a53695a
Matthew Perrot <Matthew@MacBook-Pro.local>
2022-11-29 18:56:52 -0800

    Added to  main page

--- event-space/src/Main.js
+++ event-space/src/Main.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Main = () => {
 
 return (
-    <h2>Hello</h2>
-)
-
-}
+    <div>
+        <h2>No more worrying about FOMO! Event Space is here to help you stay up to date and secure tickets to all of the best forms of entertainment in Las Vegas!</h2>
+        <img height="500px" width="700px" src='./vegas-pic.jpg' />
+    </div>
+    );
+};
 
 export default Main
